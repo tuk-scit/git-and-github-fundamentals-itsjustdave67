49acda662f491e63c0454e92b1a3ab99573bb86b
itsjustdave67 <itsjustdave67@gmail.com>
2022-11-19 11:54:54 +0300

    connection of realtime database to display on the table.

--- TGA.js
+++ TGA.js
@@ -27,18 +27,97 @@ const menu_btn = document.getElementById("menu-btn");
 const TGAmenu_container = document.getElementById("TGAmenu-container");
 const table_rows = document.querySelectorAll('tr');
 
+getProducts();
 
 table_rows.forEach( table_row => {
     table_row.addEventListener("click",()=> {
-        let target = document.querySelectorAll('# > ul > li a');
-
+        let target = table_row.children[0].value;
+        console.log(target);
     });
 });
 
-function selectData() {
+function getProducts () {
     const dbref = ref(db);
   
-    get(child(dbref,"TheProductss/" + pname.value)).then((snapshot)=>{
+    get(child(dbref,"Counter_id")).then((snapshot)=>{
+        if(snapshot.val().Counter_id > 1) {
+           var tableId = snapshot.val().Counter_id;
+           for(var i=1; i<tableId; i++) {
+                get(child(dbref,"TheProducts/" + i)).then((snapshot)=>{
+                    var p_id = snapshot.val().p_id
+                    var category = snapshot.val().category
+                    var pname = snapshot.val().name
+                    var description = snapshot.val().description
+                    var image_file = snapshot.val().image_file
+                    var minimum_bid = snapshot.val().minimum_bid
+                    var bid_deadline = snapshot.val().bid_deadline
+
+                    fillTable(p_id,category,pname,description,image_file,minimum_bid,bid_deadline)
+                });
+           }
+        }
+        else {
+            alert ("No data found");
+        }
+    }); 
+}
+
+function fillTable (p_id,category,pname,description,image_file,minimum_bid,bid_deadline) {
+    var tbody = document.getElementById("tbody");
+    var trow = document.createElement("tr");
+    trow.className = 'product-row';
+
+    var p0 = document.createElement("p");
+    var p1 = document.createElement("p");
+    var p2 = document.createElement("p");
+    var p3 = document.createElement("p");
+    var p4 = document.createElement("p");
+    var p5 = document.createElement("p");
+
+    p0.innerHTML = p_id;
+    p1.innerHTML = category;
+    p2.innerHTML = pname;
+    p3.innerHTML = image_file;
+    p4.innerHTML = minimum_bid;
+    p5.innerHTML = bid_deadline; 
+
+    var td0 = document.createElement("td");
+    var td1 = document.createElement("td");
+    var td2 = document.createElement("td");
+    var td3 = document.createElement("td");
+    var td4 = document.createElement("td");
+    var td5 = document.createElement("td");
+
+    td0.className = 'product-id';
+    td1.className = 'product-img-con';
+    td2.className = 'product-category';
+    td3.className = 'product-name';
+    td4.className = 'product-min-bid';
+    td5.className = 'product-bid-deadline';
+
+    td0.appendChild(p0);
+    td1.appendChild(p1);
+    td2.appendChild(p2);
+    td3.appendChild(p3);
+    td4.appendChild(p4);
+    td5.appendChild(p5);
+    
+    trow.appendChild(td0);
+    trow.appendChild(td1);
+    trow.appendChild(td2);
+    trow.appendChild(td3);
+    trow.appendChild(td4);
+    trow.appendChild(td5);
+
+    tbody.appendChild(trow);
+
+}
+
+function selectData() {
+    const dbref = ref(db);
+    
+
+    get(child(dbref,"TheProducts/" + pname.value)).then((snapshot)=>{
       if(snapshot.exists()) {
         alert(snapshot.val().Username);
       }
@@ -48,8 +127,9 @@ function selectData() {
     })
     .catch((error)=>{
       alert("Unsuccessful, Error!<br/>"+error.message);
-    })
-  }
+    });
+}
+
 
 
 
